e9b1109a986d6eb05f5c8c9d6a97b836f213e9df
Alex <1987lex@gmail.com>
2025-11-14 23:30:11 +0100

    add webhook url

--- config.py
+++ config.py
@@ -3,27 +3,33 @@ from os import getenv
 
 
 class TG:
-	def __init__(self):
-		dotenv.load_dotenv()
-		self.token = getenv("TELEGRAM_TOKEN")
-		self.chat_id = getenv("TELEGRAM_CHAT_ID")
+    def __init__(self):
+        dotenv.load_dotenv()
+        self.token = getenv("TELEGRAM_TOKEN")
+        self.chat_id = getenv("TELEGRAM_CHAT_ID")
 
 
 class DB:
-	def __init__(self):
-		dotenv.load_dotenv()
-		self.host = getenv("DB_HOST")
-		self.port = getenv("DB_PORT")
-		self.user = getenv("DB_USER")
-		self.password = getenv("DB_PASSWORD")
-		self.name = getenv("DB_NAME")
+    def __init__(self):
+        dotenv.load_dotenv()
+        self.host = getenv("DB_HOST")
+        self.port = getenv("DB_PORT")
+        self.user = getenv("DB_USER")
+        self.password = getenv("DB_PASSWORD")
+        self.name = getenv("DB_NAME")
 
 
 class RedisConf():
-	""" Redis settings """
-	def __init__(self) -> None:
-		dotenv.load_dotenv()
-		self.host = getenv('REDIS_HOST')
-		self.port = int(getenv('REDIS_PORT', 6379))
-		self.db = int(getenv('REDIS_DB', 0))
-		self.channel = getenv('REDIS_CHANNEL')
+    """ Redis settings """
+    def __init__(self) -> None:
+        dotenv.load_dotenv()
+        self.host = getenv('REDIS_HOST')
+        self.port = int(getenv('REDIS_PORT', 6379))
+        self.db = int(getenv('REDIS_DB', 0))
+        self.channel = getenv('REDIS_CHANNEL')
+
+
+class Webhook():
+    def __init__(self):
+        dotenv.load_dotenv()
+        self.url = getenv("WEBHOOK_URL")
